f26e0525f5cc21c74006d2bb70cb1bc9e2a4db42
Michael Li <michaelxkayo@hotmail.com>
2025-10-21 23:29:12 -0400

    Integrate Clip to Predict Service

--- context/services/predict_service.py
+++ context/services/predict_service.py
@@ -1,2 +1,10 @@
-def predict_service():
-    pass
+from context.image_classification_model.clip_model import ClipModel
+from PIL import Image
+import io
+
+def predict_service(image):
+    clip = ClipModel()
+    bytes_data = image.read()
+    img = Image.open(io.BytesIO(bytes_data)).convert("RGB")
+    clip.predict(img)
+

ed9e3c7813ad9c6b8cb923917c323ead47cb2ead
Michael Li <michaelxkayo@hotmail.com>
2025-11-02 14:20:16 -0500

    Make prospects label set more accurate

--- context/const/baseball_const.py
+++ context/const/baseball_const.py
@@ -1,5 +1,5 @@
 BOWMAN_CHROME_PARALLELS_LIST = [
-    # Shimmer parallels
+    # --- Shimmer parallels ---
     ["purple", "shimmer"],
     ["blue", "shimmer"],
     ["green", "shimmer"],
@@ -7,86 +7,237 @@ BOWMAN_CHROME_PARALLELS_LIST = [
     ["orange", "shimmer"],
     ["red", "shimmer"],
     ["black", "shimmer"],
+    ["aqua", "shimmer"],
+    ["fuchsia", "shimmer"],
+    ["yellow", "shimmer"],
 
-    # Wave parallels
+    # --- Wave parallels ---
     ["blue", "wave"],
     ["green", "wave"],
     ["gold", "wave"],
     ["orange", "wave"],
     ["red", "wave"],
     ["black", "wave"],
+    ["fuchsia", "wave"],
+    ["aqua", "wave"],
+    ["purple", "wave"],
+    ["yellow", "wave"],
 
-    # Speckle / Lava
+    # --- Speckle / Lava ---
     ["speckle"],
     ["aqua", "lava"],
     ["blue", "lava"],
     ["gold", "lava"],
     ["red", "lava"],
     ["black", "lava"],
+    ["orange", "lava"],
+    ["purple", "lava"],
+    ["fuchsia", "lava"],
 
-    # Atomic / Cracked Ice
+    # --- Atomic / Cracked Ice ---
     ["atomic"],
     ["orange", "atomic"],
+    ["red", "atomic"],
 
-    # Mojo (Mega Box exclusive)
+    # --- Reptilian ---
+    ["green", "reptilian"],
+    ["orange", "reptilian"],
+    ["red", "reptilian"],
+    ["blue", "reptilian"],
+    ["black", "reptilian"],
+
+    # --- Mojo (Mega Box exclusive) ---
     ["gold", "mojo"],
     ["orange", "mojo"],
     ["red", "mojo"],
     ["black", "mojo"],
+    ["aqua", "mojo"],
+    ["green", "mojo"],
+    ["purple", "mojo"],
+    ["fuchsia", "mojo"],
+    ["yellow", "mojo"],
 
-    # Sapphire (special release)
+    # --- X-Fractors (checkerboard grid) ---
+    ["x-fractor"],
+    ["aqua", "x-fractor"],
+    ["yellow", "x-fractor"],
+    ["orange", "x-fractor"],
+    ["red", "x-fractor"],
+    ["black", "x-fractor"],
+
+    # --- Mini-Diamond / RayWave ---
+    ["mini", "diamond"],
+    ["black", "mini", "diamond"],
+    ["raywave"],
+    ["purple", "raywave"],
+    ["red", "raywave"],
+
+    # --- Sapphire (special release) ---
+    ["sapphire"],
     ["sapphire", "blue"],
     ["sapphire", "orange"],
     ["sapphire", "red"],
-    ["sapphire"],
-    ["padparadscha"],
+    ["sapphire", "gold"],
+    ["sapphire", "green"],
+    ["sapphire", "yellow"],
 
-    # Other less common Chrome parallels
-    ["fuchsia"],
-    ["yellow"],
+    # --- Other rare / special parallels ---
+    ["padparadscha"],
     ["rose", "gold"],
-    ["black"],
-    ["aqua"],
-    ["x-fractor"],
-    ["platinum"],
     ["canary", "diamond"],
+    ["platinum"],
+    ["pink"],
+    ["black"],
     ["purple"],
     ["blue"],
     ["green"],
+    ["aqua"],
+    ["yellow"],
     ["gold"],
     ["orange"],
     ["red"],
+    ["fuchsia"],
     ["superfractor"],
-    ["refractor"],
 
-    ["mojo"],
+    # --- Base ---
     ["base"],
 ]
 
+
+BOWMAN_CHROME_PARALLELS_DICT = {
+    # --- Shimmer parallels ---
+    "shimmer":
+        [["purple", "shimmer"],
+        ["blue", "shimmer"],
+        ["green", "shimmer"],
+        ["gold", "shimmer"],
+        ["orange", "shimmer"],
+        ["red", "shimmer"],
+        ["black", "shimmer"],
+        ["aqua", "shimmer"],
+        ["fuchsia", "shimmer"],
+        ["yellow", "shimmer"]],
+
+    # --- Wave parallels ---
+    "wave": [["blue", "wave"],
+    ["green", "wave"],
+    ["gold", "wave"],
+    ["orange", "wave"],
+    ["red", "wave"],
+    ["black", "wave"],
+    ["fuchsia", "wave"],
+    ["aqua", "wave"],
+    ["purple", "wave"],
+    ["yellow", "wave"]],
+
+    # --- Speckle / Lava ---
+    "speckle": ["speckle"],
+    "lava": [["aqua", "lava"],
+    ["blue", "lava"],
+    ["gold", "lava"],
+    ["red", "lava"],
+    ["black", "lava"],
+    ["orange", "lava"],
+    ["purple", "lava"],
+    ["fuchsia", "lava"]],
+
+    # --- Atomic / Cracked Ice ---
+    "atomic":
+    [["orange", "atomic"],
+    ["red", "atomic"]],
+
+    # --- Mojo (Mega Box exclusive) ---
+    "mojo": [["gold", "mojo"],
+    ["orange", "mojo"],
+    ["red", "mojo"],
+    ["black", "mojo"],
+    ["aqua", "mojo"],
+    ["green", "mojo"],
+    ["purple", "mojo"],
+    ["fuchsia", "mojo"],
+    ["yellow", "mojo"]],
+
+    # --- X-Fractors (checkerboard grid) ---
+    "x-fractor": [["aqua", "x-fractor"],
+    ["yellow", "x-fractor"],
+    ["orange", "x-fractor"],
+    ["red", "x-fractor"],
+    ["black", "x-fractor"]],
+
+    # --- Mini-Diamond / RayWave ---
+    "mini diamond":[
+    ["black", "mini", "diamond"]],
+
+    "raywave":
+    [["purple", "raywave"],
+    ["red", "raywave"]],
+
+    # --- Sapphire (special release) ---
+    "sapphire":
+    [["sapphire", "blue"],
+    ["sapphire", "orange"],
+    ["sapphire", "red"],
+    ["sapphire", "gold"],
+    ["sapphire", "green"],
+    ["sapphire", "yellow"], ["padparadscha"]],
+
+    # --- Reptilian ---
+    "reptilian": [["green", "reptilian"],
+    ["orange", "reptilian"],
+    ["red", "reptilian"],
+    ["blue", "reptilian"],
+    ["black", "reptilian"]],
+
+    # --- Other rare / special parallels ---
+    "refractor": [["rose", "gold", 'refractor'],
+    ["canary", "diamond", 'refractor'],
+    ["platinum", 'refractor'],
+    ["pink", 'refractor'],
+    ["black", 'refractor'],
+    ["purple", 'refractor'],
+    ["blue", 'refractor'],
+    ["green", 'refractor'],
+    ["aqua", 'refractor'],
+    ["yellow", 'refractor'],
+    ["gold", 'refractor'],
+    ["orange", 'refractor'],
+    ["red", 'refractor'],
+    ["fuchsia", 'refractor'],
+    ["superfractor"]],
+}
+
+
 BOWMAN_PAPER_PARALLELS_LIST = [
-    # Standard paper base
     "base",
 
-    # Common colors
+    # --- Common colors ---
     "sky blue",      # /499
+    "silver",        # /499 alt name
+    "purple",        # /250
     "blue",          # /150
+    "neon green",    # /399
+    "lime green",    # /399 alt name
     "green",         # /99
     "gold",          # /50
     "orange",        # /25
     "red",           # /5
     "black",         # /1 in some sets
 
-    # Camo & retail exclusives
-    "camo",
-    "pink",          # /199 or retail exclusive
+    # --- Retail exclusives ---
+    "pink",          # /199 or retail-only
+    "fuchsia",       # /299 or /199
+    "aqua",          # /125 or /199 depending on year
+    "teal",          # /199 (newer tone)
     "yellow",        # /75 or retail exclusive
     "gray",
-    "aqua",          # /125 or /199 depending on year
-    "fuchsia",       # /299 or /199
-    "violet",        # special runs, /250
+    "gray pattern",  # retail/fanatics variation
+    "camo",          # unnumbered retail
+    "lavender",      # /299 new 2024 tone
 
-    # Rare / special releases
-    "silver",        # /499
-    "platinum",      # 1/1
-    "canary diamond",# 1/1
+    # --- Special limited releases ---
+    "rainbow foil",  # retail or 1st Edition foil stock
+    "ice",           # /99 or /50 Paper Ice pattern
+    "white",         # 1/1 limited paper tone
+    "platinum",      # true 1/1
+    "canary diamond" # 1/1 gem variant
 ]
